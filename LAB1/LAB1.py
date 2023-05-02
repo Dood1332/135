0706823b339180a3bf6db0db7e9065c214bac162
Charlie Nguyen <captaincharlie2000@gmail.com>
2023-05-02 02:03:22 -0700

    png

--- LAB1/LAB1.py
+++ LAB1/LAB1.py
@@ -77,139 +77,139 @@ for i in range(0, 1):
         calibrated_science_data[i][calibrated_science_data[i] < 0] = 0
         calibrated_calibration_data[i][calibrated_calibration_data[i] < 0] = 0
 
-plt.imshow(calibrated_science_data[1], interpolation='nearest', vmin=0, vmax=50)
+plt.imshow(calibrated_science_data[5], interpolation='nearest', vmin=0, vmax=50)
 plt.colorbar()
-#plt.savefig('calibrated_calibratedB.pdf')
+plt.savefig('calibrated_calibratedM67R.png')
 plt.show()
 
-#Making the HR Diagram
+# #Making the HR Diagram
 
-def bg_subtraction(data):
-    bkg = sep.Background(data)
+# def bg_subtraction(data):
+#     bkg = sep.Background(data)
     
-    bkg_image = bkg.back()
-    bkg_rms = bkg.rms()
+#     bkg_image = bkg.back()
+#     bkg_rms = bkg.rms()
 
-    data_sub = data - bkg
+#     data_sub = data - bkg
 
-    objects = sep.extract(data_sub, 1.5, err=bkg.globalrms)
+#     objects = sep.extract(data_sub, 1.5, err=bkg.globalrms)
 
     
-    print('Objects detected:',len(objects))
+#     print('Objects detected:',len(objects))
 
-    fig, ax = plt.subplots()
-    m, s = np.mean(data_sub), np.std(data_sub)
-    im = ax.imshow(data_sub, interpolation='nearest', cmap='gray',
-                    vmin=0, vmax=50)
+#     fig, ax = plt.subplots()
+#     m, s = np.mean(data_sub), np.std(data_sub)
+#     im = ax.imshow(data_sub, interpolation='nearest', cmap='gray',
+#                     vmin=0, vmax=50)
     
-    #limit ellipse size
-    min_size = 1
-    max_size = 75
+#     #limit ellipse size
+#     min_size = 1
+#     max_size = 75
 
-    for i in range(len(objects)):
-        a = objects['a'][i]
-        b = objects['b'][i]
-        if a > min_size and a < max_size and b > min_size and b < max_size:
-            e = Ellipse(xy=(objects['x'][i], objects['y'][i]),
-                    width=6*objects['a'][i],
-                    height=6*objects['b'][i],
-                    angle=objects['theta'][i] * 180. / np.pi)
-            e.set_facecolor('none')
-            e.set_edgecolor('red')
-            ax.add_artist(e)
-    plt.show()
+#     for i in range(len(objects)):
+#         a = objects['a'][i]
+#         b = objects['b'][i]
+#         if a > min_size and a < max_size and b > min_size and b < max_size:
+#             e = Ellipse(xy=(objects['x'][i], objects['y'][i]),
+#                     width=6*objects['a'][i],
+#                     height=6*objects['b'][i],
+#                     angle=objects['theta'][i] * 180. / np.pi)
+#             e.set_facecolor('none')
+#             e.set_edgecolor('red')
+#             ax.add_artist(e)
+#     plt.show()
 
-#bg_subtraction(calibrated_calibration_data[0])
-
-
-def mag_atm_ext(filters, m_atm_ext):
-    calibration_airmass = []
-    for i in range(filters[0], filters[1]):
-        filename = f"{filters[2]}/d{i}.fits"
-        with fits.open(filename) as hdulist:
-            data = hdulist[0].header['AIRMASS']
-            calibration_airmass.append(data)
-    mag_airmass = np.median(calibration_airmass) * m_atm_ext
-    return mag_airmass
-
-def flux(data):
-    zp = 25.0
-    gain = 1.0
-    bkg = sep.Background(data)
-    data_sub = data - bkg
-    objects = sep.extract(data_sub, 1.5, err=bkg.globalrms)
-    flux, flux_err, flag = sep.sum_circle(data_sub, objects['x'], objects['y'], 50, err=bkg.globalrms, gain=gain)
-    return flux
-
-F_sciB = flux(calibrated_science_data[0])
-F_sciV = flux(calibrated_science_data[1])
-F_sciR = flux(calibrated_science_data[2])
-
-F_caliB = flux(calibrated_calibration_data[0])
-F_caliV = flux(calibrated_calibration_data[1])
-F_caliR = flux(calibrated_calibration_data[2])
-
-print(len(F_sciB),len(F_sciV),len(F_sciR))
+# #bg_subtraction(calibrated_calibration_data[0])
 
 
-# Atm sci
-m_atm_ext_sciB = .234
-m_atm_ext_sciB = mag_atm_ext(science_filters[0], m_atm_ext_sciB)
+# def mag_atm_ext(filters, m_atm_ext):
+#     calibration_airmass = []
+#     for i in range(filters[0], filters[1]):
+#         filename = f"{filters[2]}/d{i}.fits"
+#         with fits.open(filename) as hdulist:
+#             data = hdulist[0].header['AIRMASS']
+#             calibration_airmass.append(data)
+#     mag_airmass = np.median(calibration_airmass) * m_atm_ext
+#     return mag_airmass
 
-m_atm_ext_sciV = .460
-m_atm_ext_sciV = mag_atm_ext(science_filters[1], m_atm_ext_sciV)
+# def flux(data):
+#     zp = 25.0
+#     gain = 1.0
+#     bkg = sep.Background(data)
+#     data_sub = data - bkg
+#     objects = sep.extract(data_sub, 1.5, err=bkg.globalrms)
+#     flux, flux_err, flag = sep.sum_circle(data_sub, objects['x'], objects['y'], 50, err=bkg.globalrms, gain=gain)
+#     return flux
 
-m_atm_ext_sciR = .094
-m_atm_ext_sciR = mag_atm_ext(science_filters[2], m_atm_ext_sciR)
-print(m_atm_ext_sciB, m_atm_ext_sciV, m_atm_ext_sciR)
+# F_sciB = flux(calibrated_science_data[0])
+# F_sciV = flux(calibrated_science_data[1])
+# F_sciR = flux(calibrated_science_data[2])
 
-#Dust sci
-m_dust_ext_sciB = 0.092
-m_dust_ext_sciV = 0.083
-m_dust_ext_sciR = 0.060
+# F_caliB = flux(calibrated_calibration_data[0])
+# F_caliV = flux(calibrated_calibration_data[1])
+# F_caliR = flux(calibrated_calibration_data[2])
 
-#B 478.5
-#V 386.2
-#R 710.0
-
-#Atm cali
-m_atm_ext_caliB = mag_atm_ext(calibration_filters[0], m_atm_ext_sciB)
-m_atm_ext_caliV = mag_atm_ext(calibration_filters[1], m_atm_ext_sciV)
-m_atm_ext_caliR = mag_atm_ext(calibration_filters[2], m_atm_ext_sciR)
-print(m_atm_ext_caliB, m_atm_ext_caliV, m_atm_ext_caliR)
-
-#Dust cali
-m_dust_ext_caliB = 0.173
-m_dust_ext_caliV = 0.148
-m_dust_ext_caliR = 0.107
+# print(len(F_sciB),len(F_sciV),len(F_sciR))
 
 
-# m_v -mI
-m_I_B = -2.5 * np.log(F_caliB) - m_atm_ext_caliB - m_dust_ext_caliB
-m_I_V = -2.5 * np.log(F_caliV) - m_atm_ext_caliV - m_dust_ext_caliV
-m_I_R = -2.5 * np.log(F_caliR) - m_atm_ext_caliR - m_dust_ext_caliR
+# # Atm sci
+# m_atm_ext_sciB = .234
+# m_atm_ext_sciB = mag_atm_ext(science_filters[0], m_atm_ext_sciB)
 
-m_B = -2.5 * np.log(F_caliB)
-m_V = -2.5 * np.log(F_caliV)
-m_R = -2.5 * np.log(F_caliR)
+# m_atm_ext_sciV = .460
+# m_atm_ext_sciV = mag_atm_ext(science_filters[1], m_atm_ext_sciV)
 
-m_zpB = m_B - m_I_B
-m_zpV = m_V - m_I_V
-m_zpR = m_R - m_I_R
+# m_atm_ext_sciR = .094
+# m_atm_ext_sciR = mag_atm_ext(science_filters[2], m_atm_ext_sciR)
+# print(m_atm_ext_sciB, m_atm_ext_sciV, m_atm_ext_sciR)
 
-# m_caliB = F_caliB - m_I_B
-# m_caliV = F_caliV - m_I_V
-# m_caliR = F_caliR - m_I_R
+# #Dust sci
+# m_dust_ext_sciB = 0.092
+# m_dust_ext_sciV = 0.083
+# m_dust_ext_sciR = 0.060
 
-print(len(m_caliB), len(m_caliV), len(m_caliR))
+# #B 478.5
+# #V 386.2
+# #R 710.0
 
-print(len(m_I_B), len(m_I_V), len(m_I_R))
+# #Atm cali
+# m_atm_ext_caliB = mag_atm_ext(calibration_filters[0], m_atm_ext_sciB)
+# m_atm_ext_caliV = mag_atm_ext(calibration_filters[1], m_atm_ext_sciV)
+# m_atm_ext_caliR = mag_atm_ext(calibration_filters[2], m_atm_ext_sciR)
+# print(m_atm_ext_caliB, m_atm_ext_caliV, m_atm_ext_caliR)
 
-# x = mag_int_sciB
-# y = mag_int_sciV
+# #Dust cali
+# m_dust_ext_caliB = 0.173
+# m_dust_ext_caliV = 0.148
+# m_dust_ext_caliR = 0.107
 
-# y = mag_int_sciR
+
+# # m_v -mI
+# m_I_B = -2.5 * np.log(F_caliB) - m_atm_ext_caliB - m_dust_ext_caliB
+# m_I_V = -2.5 * np.log(F_caliV) - m_atm_ext_caliV - m_dust_ext_caliV
+# m_I_R = -2.5 * np.log(F_caliR) - m_atm_ext_caliR - m_dust_ext_caliR
+
+# m_B = -2.5 * np.log(F_caliB)
+# m_V = -2.5 * np.log(F_caliV)
+# m_R = -2.5 * np.log(F_caliR)
+
+# m_zpB = m_B - m_I_B
+# m_zpV = m_V - m_I_V
+# m_zpR = m_R - m_I_R
+
+# # m_caliB = F_caliB - m_I_B
+# # m_caliV = F_caliV - m_I_V
+# # m_caliR = F_caliR - m_I_R
+
+# print(len(m_caliB), len(m_caliV), len(m_caliR))
+
+# print(len(m_I_B), len(m_I_V), len(m_I_R))
+
+# # x = mag_int_sciB
+# # y = mag_int_sciV
+
+# # y = mag_int_sciR
 
 
-plt.scatter()
-plt.show()
+# plt.scatter()
+# plt.show()
