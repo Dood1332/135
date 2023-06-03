be212f3706c1a59195853d0c3a42634141d0ba03
Charlie Nguyen <captaincharlie2000@gmail.com>
2023-06-02 22:19:01 -0700

    aaa

--- LAB3/LAB3.py
+++ LAB3/LAB3.py
@@ -39,30 +39,38 @@ def csv_plot(csv_file):
 
 csv_files = ['TRACE051.CSV', 'TRACE052.CSV', 'TRACE053.CSV', 'TRACE054.CSV', 'TRACE055.CSV',
              'TRACE056.CSV', 'TRACE057.CSV', 'TRACE058.CSV', 'TRACE059.CSV',]
-
-x, y = csv_plot('TRACE051.CSV')
-x = np.array(x)
-y = np.array(y)
-#print(x)
-def func(x, a, b,c,d,e):
-    return a*np.exp(-((x-b)**2)/(c))+d*x+e
-#next line from curve_fit documentation. p0=(guess values)
-popt, pcov = curve_fit(func, x, y, p0=(-103,1.42025*1e9,0.0050*1e9,0,7.085*1e-8))# bounds=(0, [.5, ,.5, 1., 0.5]))
-plt.plot(x, func(x, *popt), 'r-',
-         label='fit: a=%f, b=%f, c=%f, d=%f, e=%f' % tuple(popt))
-#feeding my x values into my function with guess values
-guess = func(x, a=-103, b=1.42025*1e9, c=4.0*1e9, d=0, e=7.085*1e-8)
-plt.plot(x, y, 'b-', label='data')
-#plt.plot(x, guess, 'g-', label='guess')
-
-plt.figure(figsize = (10,5))
-plt.xlabel('Frequency (Hz)')
-plt.ylabel('Amplitude (dBm)')
-
 for csv_file in csv_files:
     x, y = csv_plot(csv_file)
-    plt.plot(x, y)
+    x = np.array(x)
+    y = np.array(y)
+    a = np.max(y)
+    print(a)
+    max_index = np.argmax(y)
+    b = x[max_index]
 
-plt.legend(['051', '052', '053', '054', '055', '056', '057', '058', '059'])
-#plt.savefig('Plot.png')
-plt.show()
+    def func(x, a, b,c,d,e):
+        return a*np.exp(-((x-b)**2)/(c))+d*x+e
+    #next line from curve_fit documentation. p0=(guess values)
+    popt, pcov = curve_fit(func, x, y, p0=(a, b, 0.0050*1e9, 0 , 7.085*1e-8))# bounds=(0, [.5, ,.5, 1., 0.5]))
+    plt.figure(figsize = (10,5))
+    plt.xlabel('Frequency (Hz)')
+    plt.ylabel('Amplitude (dBm)')
+    plt.plot(x, func(x, *popt), 'r-',
+            label='fit: a=%f, b=%f, c=%f, d=%f, e=%f' % tuple(popt))
+    #feeding my x values into my function with guess values
+    plt.plot(x, y, 'b-', label='data')
+    plt.legend(['Fitted Curve', f'{csv_file[0:8]}'])
+    plt.savefig(f'{csv_file}.png')
+    plt.show()
+
+# plt.figure(figsize = (10,5))
+# plt.xlabel('Frequency (Hz)')
+# plt.ylabel('Amplitude (dBm)')
+
+# for csv_file in csv_files:
+#     x, y = csv_plot(csv_file)
+#     plt.plot(x, y)
+
+# plt.legend(['051', '052', '053', '054', '055', '056', '057', '058', '059'])
+# #plt.savefig('Plot.png')
+# plt.show()
